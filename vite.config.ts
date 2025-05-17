3f6125877665b4ec05cb89fb62d02d50d8a3e469
durumi99 <kms990615@naver.com>
2025-05-17 17:28:09 +0900

    Merge branch 'main' of https://github.com/nerdinary-hackathon-8th/frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-// https://vite.dev/config/
+
 export default defineConfig({
 	plugins: [react(), svgr()],
-	build: {
-		outDir: 'dist',
-		assetsDir: 'assets',
-		emptyOutDir: true,
+
+	resolve: {
+		alias: [
+			{ find: '@ts', replacement: '/src/ts' },
+			{ find: '@base', replacement: '/src/base' },
+			{ find: '@utils', replacement: '/src/utils' },
+			{ find: '@hooks', replacement: '/src/hooks' },
+			{ find: '@pages', replacement: '/src/pages' },
+			{ find: '@router', replacement: '/src/router' },
+			{ find: '@layout', replacement: '/src/layout' },
+			{ find: '@common', replacement: '/src/common' },
+			{ find: '@recoils', replacement: '/src/recoils' },
+			{ find: '@constants', replacement: '/src/constants' },
+			{ find: '@controllers', replacement: '/src/controllers' },
+			{ find: '@components', replacement: '/src/components' },
+			{ find: '@styles', replacement: '/src/styles' },
+			{ find: '@assets', replacement: '/src/assets' },
+		],
 	},
 });
