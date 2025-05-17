3fa4c643626153d628f949c50669313d69954544
durumi99 <kms990615@naver.com>
2025-05-17 23:30:23 +0900

    [feat] js 삭제

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { RouterProvider } from "react-router-dom";
-import "./App.css";
-import { router } from "./router";
-function App() {
-    return _jsx(RouterProvider, { router: router });
-}
-export default App;
