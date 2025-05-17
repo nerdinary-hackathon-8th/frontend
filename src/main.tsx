d72086f945660fa6872ae70f23dfc704981784ab
durumi99 <kms990615@naver.com>
2025-05-17 17:24:54 +0900

    test4

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	<BrowserRouter basename="/frontend">
+	<StrictMode>
 		<App />
-	</BrowserRouter>
+	</StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/frontend/',
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	build: {
 		outDir: 'dist',
 		assetsDir: 'assets',
