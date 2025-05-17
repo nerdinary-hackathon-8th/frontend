ac10889b071d586183cede156da089a054e63faf
kimyounghee425 <doctorsean0425@gmail.com>
2025-05-17 18:44:14 +0900

    [feat] footer 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import MenuFooter from "./layout/Mainlayout/MenuFooter";
 
 function App() {
-  return (
-    <div>
-      <h1>App Page</h1>
-    </div>
-  );
+  const currentMenuIndex = 1;
+    return (
+        <div>
+            <h1>App Page</h1>
+            <MenuFooter currentMenuIndex={currentMenuIndex} />
+        </div>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import Router from './router/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Router />
 	</StrictMode>
 );
