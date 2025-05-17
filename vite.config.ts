3862da62e23c08efaf781cebd01f929cc9d55e9a
durumi99 <kms990615@naver.com>
2025-05-17 17:21:45 +0900

    test3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend/',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  }
-})
+	base: '/frontend/',
+	plugins: [react()],
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		emptyOutDir: true,
+	},
+});
