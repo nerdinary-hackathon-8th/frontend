6bb62f9d67bb57c452a43c2519bafdc36d5eec0f
kimyounghee425 <doctorsean0425@gmail.com>
2025-05-17 19:59:51 +0900

    path 추가

--- src/components/FoodList/FoodList.tsx
+++ src/components/FoodList/FoodList.tsx
@@ -4,24 +4,24 @@ import { useNavigate } from "react-router-dom";
 import FoodCard from "./FoodCard";
 
 export default function FoodList() {
-    //   const navigate = useNavigate();
+      const navigate = useNavigate();
     const [currentId, setCurrentId] = useState<number | null>(null);
 
     // 임시 더미 데이터 (FoodCard를 위한 아이디와 이름 등)
     // 필요하면 FoodCard prop에 맞게 수정하세요
     const foodCards = [
         { id: 1, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 2, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 3, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 4, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 5, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 6, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 7, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 8, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 9, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 10, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 11, name: "해산물", icon: "/path/to/seafood.png" },
-        { id: 12, name: "해산물", icon: "/path/to/seafood.png" },
+        { id: 2, name: "축산/계란", icon: "/path/to/seafood.png" },
+        { id: 3, name: "채소", icon: "/path/to/seafood.png" },
+        { id: 4, name: "과일", icon: "/path/to/seafood.png" },
+        { id: 5, name: "유제품", icon: "/path/to/seafood.png" },
+        { id: 6, name: "조미료/향신료", icon: "/path/to/seafood.png" },
+        { id: 7, name: "과자/시리얼", icon: "/path/to/seafood.png" },
+        { id: 8, name: "견과류", icon: "/path/to/seafood.png" },
+        { id: 9, name: "간편식/밀키트", icon: "/path/to/seafood.png" },
+        { id: 10, name: "가공식품", icon: "/path/to/seafood.png" },
+        { id: 11, name: "건강식품", icon: "/path/to/seafood.png" },
+        { id: 12, name: "기타", icon: "/path/to/seafood.png" },
     ];
 
     const handleCardClick = (id: number) => {
@@ -41,7 +41,7 @@ export default function FoodList() {
                             name={card.name}
                             icon={card.icon}
                             isSelected={card.id === currentId}
-                            onClick={() => handleCardClick(card.id)}
+                            onClick={() => navigate(``)}
                         />
                     ))}
                 </div>
---
+++ src/pages/RegisterFood/RegisterFood.tsx
@@ -0,0 +1,3 @@
+export default function RegisterFood() {
+    return <>음식 등록</>
+}
